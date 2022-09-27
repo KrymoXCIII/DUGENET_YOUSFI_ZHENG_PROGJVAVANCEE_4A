1742e42d91180b2de63acc2d739893ccb47bafc5
Denis ZHENG <dzheng2@myges.fr>
2022-09-27 15:35:40 +0200

    CHOOSE AGENT MODE

--- DUGENET_YOUSFI_ZHENG_PROGJVAVANCEE_4A/Assets/Scripts/ChooseMode.cs
+++ DUGENET_YOUSFI_ZHENG_PROGJVAVANCEE_4A/Assets/Scripts/ChooseMode.cs
@@ -1,22 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class ChooseMode : MonoBehaviour
 {
-    public CharacterMovement CM1;
-    public CharacterMovement CM2;
-    
-    public void chooseRandom()
+    public static ChooseMode instance;
+    public int CM1;
+    public int CM2;
+
+    private void Awake()
     {
-        CM1.AgentMode = 0;
+        if (instance != null)
+        {
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(instance);
     }
-    public void choosePlayer()
+
+    public void choose1Random()
     {
-        CM1.AgentMode = 1;
+        CM1= 0;
     }
-    public void chooseMCTS()
+    public void choose1Player()
     {
-        CM1.AgentMode = 2;
+        CM1 = 1;
+    }
+    public void choose1MCTS()
+    {
+        CM1= 2;
+    }
+    public void choose2Random()
+    {
+        CM2= 0;
+    }
+    public void choose2Player()
+    {
+        CM2= 1;
+    }
+    public void choose2MCTS()
+    {
+        CM2= 2;
     }
 }
