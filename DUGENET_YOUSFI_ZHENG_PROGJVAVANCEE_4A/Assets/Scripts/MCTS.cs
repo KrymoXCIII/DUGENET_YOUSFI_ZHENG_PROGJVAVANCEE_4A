a551f63cc32403ad1c59c4814fbd0853b2188819
KrymoXCIII <karym.yousfi@gmail.com>
2022-09-28 15:51:26 +0200

    mcts

---
+++ DUGENET_YOUSFI_ZHENG_PROGJVAVANCEE_4A/Assets/Scripts/MCTS.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game 
+{
+    void startState()
+    {
+        
+        
+    }
+
+    void nextState()
+    {
+        
+    }
+
+    void possiblePlays()
+    {
+        
+    }
+
+    void winner()
+    {
+        
+    }
+    
+}
+
+public class MCTS
+{
+    
+}
