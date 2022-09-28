b117cdb41a2287b9215b42a22e47bdd4b052413e
Soorry <77273729+Soorry@users.noreply.github.com>
2022-09-28 11:06:41 +0200

    collision player wall

--- DUGENET_YOUSFI_ZHENG_PROGJVAVANCEE_4A/Assets/Scripts/PlayerBomberman.cs
+++ DUGENET_YOUSFI_ZHENG_PROGJVAVANCEE_4A/Assets/Scripts/PlayerBomberman.cs
@@ -18,8 +18,10 @@ public class PlayerBomberman : MonoBehaviour
 
     public Bomb playerBomb;
 
-    public float collisionRadius =.1f;
+
     [SerializeField]public GameObject ScoreBoard;
+    public float collisionRadius =1f;
+
     
     // Start is called before the first frame update
     void Start()
@@ -63,82 +65,51 @@ public class PlayerBomberman : MonoBehaviour
     public void MovePlayerUp(GameObject obj, Transform model)
     {
         model.transform.rotation = Quaternion.Euler(0, 90, 0);
-        if (gameObject.name == "EnemyBomberman")
-        {
-            
-            var dir = new Vector3(speed, 0, 0) * Time.deltaTime;
-            if(!collisionPlayer(obj.transform.position+dir))
-                obj.transform.Translate(dir);
-        }
-        else if (gameObject.name == "CharacterBomberman")
-        {
-           
-            var dir = new Vector3(-speed, 0, 0) * Time.deltaTime;
-            if(!collisionPlayer(obj.transform.position+dir))
-                obj.transform.Translate(dir);
-        }
 
-        
-        
+            var dir = (new Vector3(0, 0, speed)* Time.deltaTime)+transform.position;
+            if (!collisionPlayer(dir))
+                transform.position = dir;
+ 
     }
     public void MovePlayerDown(GameObject obj, Transform model)
     {
         model.transform.rotation = Quaternion.Euler(0,-90,0);
-        if (gameObject.name == "EnemyBomberman")
-        {
-            var dir = new Vector3(-speed,0,0) * Time.deltaTime;
-            if(!collisionPlayer(obj.transform.position+dir))
-                obj.transform.Translate(dir);
-        }
-        else if (gameObject.name == "CharacterBomberman")
-        {
-            var dir = new Vector3(speed,0,0) * Time.deltaTime;
-            if(!collisionPlayer(obj.transform.position+dir))
-                obj.transform.Translate(dir);
-        }
+   
+            var dir = (new Vector3(0, 0, -speed)* Time.deltaTime)+transform.position;
+            if (!collisionPlayer(dir))
+                transform.position = dir;
+
     }
     public void MovePlayerRight(GameObject obj, Transform model)
     {
         model.transform.rotation = Quaternion.Euler(0, 180, 0);
-        if (gameObject.name == "EnemyBomberman")
-        {
-            var dir = new Vector3(0, 0, -speed) * Time.deltaTime;
-            if(!collisionPlayer(obj.transform.position+dir))
-                obj.transform.Translate(dir);
-        }
-        else if (gameObject.name == "CharacterBomberman")
-        {
-            var dir = new Vector3(0, 0, speed) * Time.deltaTime;
-            if(!collisionPlayer(obj.transform.position+dir))
-                obj.transform.Translate(dir);
-        }
+        var dir = (new Vector3(speed, 0, 0)* Time.deltaTime)+transform.position;
+        if (!collisionPlayer(dir))
+            transform.position = dir;
+
     }
     public void MovePlayerLeft(GameObject obj, Transform model)
     {
         model.transform.rotation = Quaternion.Euler(0, 0, 0);
-        if (gameObject.name == "EnemyBomberman")
-        {
-            var dir = new Vector3(0, 0, speed) * Time.deltaTime;
-            if(!collisionPlayer(obj.transform.position+dir))
-                obj.transform.Translate(dir);
-        }
-        else if (gameObject.name == "CharacterBomberman")
-        {
-            var dir = new Vector3(0, 0, -speed) * Time.deltaTime;
-            if(!collisionPlayer(obj.transform.position+dir))
-                obj.transform.Translate(dir);
-        }
+ 
+            var dir = (new Vector3(-speed, 0, 0)* Time.deltaTime)+transform.position;
+            if (!collisionPlayer(dir))
+                transform.position = dir;
+
     }
     
     public bool collisionPlayer(Vector3 checkPos)
     {
         foreach (var wall in map.walls)
         {
-            if (wall.transform.position.x + collisionRadius > checkPos.x - collisionRadius &&
-                wall.transform.position.x - collisionRadius < checkPos.x + collisionRadius &&
-                wall.transform.position.z + collisionRadius > checkPos.z - collisionRadius &&
-                wall.transform.position.z - collisionRadius < checkPos.z + collisionRadius)
+            if (wall.transform.position.x + collisionRadius > checkPos.x &&
+                wall.transform.position.x - collisionRadius < checkPos.x &&
+                wall.transform.position.z + collisionRadius > checkPos.z &&
+                wall.transform.position.z - collisionRadius < checkPos.z)
                 return true;
+            /*
+            if (Mathf.Pow(wall.transform.position.x - checkPos.x, 2) + Mathf.Pow(wall.transform.position.z - checkPos.z, 2) < collisionRadius)
+                return true;*/
         }
         return false;
     }
